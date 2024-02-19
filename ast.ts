587349a26a1629cd8ab38a3a71898c640bdd2bec
auronvila <auronvila1121@gmail.com>
2024-02-19 10:16:14 +0300

    added the ast type declarations

---
+++ ast.ts
@@ -0,0 +1,37 @@
+export type NodeType =
+  'Program'
+  | 'NumericLiteral'
+  | 'Identifier'
+  | 'BinaryExpression'
+
+// statement will not return a value
+
+export interface Statement {
+  kind: NodeType;
+}
+
+export interface Program extends Statement {
+  kind: 'Program';
+  body: Statement[];
+}
+
+// deno-lint-ignore no-empty-interface
+export interface Expression extends Statement {
+}
+
+export interface BinaryExpression extends Expression {
+  kind: 'BinaryExpression';
+  left: Expression;
+  right: Expression;
+  operator: string;
+}
+
+export interface Identifier extends Expression {
+  kind: 'Identifier';
+  symbol: string;
+}
+
+export interface NumericLiteral extends Expression {
+  kind: 'NumericLiteral';
+  value: number;
+}
